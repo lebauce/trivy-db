42caffdaee3f8d67c80260d622b64bf8c664311d
Sylvain Baubeau <lebauce@gmail.com>
2023-10-31 01:04:43 +0100

    feat: create zap logger on demand

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -5,18 +5,20 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-var Logger *zap.SugaredLogger
-
-func init() {
+var Logger = NewLazyLogger(func() (*zap.SugaredLogger, error) {
 	conf := zap.NewDevelopmentConfig()
 	conf.DisableCaller = true
 	conf.DisableStacktrace = true
 	conf.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
 
-	logger, _ := conf.Build()
-	Logger = logger.Sugar()
-}
+	logger, err := conf.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return logger.Sugar(), nil
+})
 
 func SetLogger(l *zap.SugaredLogger) {
-	Logger = l
+	Logger = NewLazyLogger(func() (*zap.SugaredLogger, error) { return l, nil })
 }
